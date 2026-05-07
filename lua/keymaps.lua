c19850475e6e7efe7f69fa7c655d11dbe191d7fb
tanreesego <tanreesego2@gmail.com>
2026-05-06 23:18:50 -0400

    made it.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,3 +3,4 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("x", "<leader>p", [["_dP]])
 -- JUST PRACTICING GIT (fake scenario): made an error woops!
 -- testign new feature
+-- second dangler
