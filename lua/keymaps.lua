e2ec4b97b87127dafa97c4fc560c400cb7b681b0
tanreesego <tanreesego2@gmail.com>
2026-05-07 00:05:41 -0400

    no changes?

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,6 +1,3 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("x", "<leader>p", [["_dP]])
--- JUST PRACTICING GIT (fake scenario): made an error woops!
--- testign new feature
--- second dangler
