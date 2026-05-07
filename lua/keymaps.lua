adc181c1dcaaf224c57b99439e0427a67ad63f8b
tanreesego <tanreesego2@gmail.com>
2026-05-06 23:04:35 -0400

    testign dangling head

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,3 +2,4 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("x", "<leader>p", [["_dP]])
 -- JUST PRACTICING GIT (fake scenario): made an error woops!
+-- testign new feature
